94b0039841106816dc09e9a72aaef53e6173ed95
MagistrAVSH <art046@hotmail.com>
2025-04-11 14:41:55 +0200

    initial commit

---
+++ cryptosteamsdk/lib/web/cryptosteamsdk.js
@@ -0,0 +1,259 @@
+var LIB = {
+  
+  //----------------------------------------
+  //-- Startup Events
+  //----------------------------------------
+  
+  gameReady: function() {
+    return window.CryptoSteamSDK.gameReady();
+  },
+
+  //----------------------------------------
+  //-- Advertisement
+  //----------------------------------------
+  
+  isAdEnabled: function(cb) {
+    window.CryptoSteamSDK.isAdEnabled().then(response => {
+        dynCall_vi(cb, response);
+    });
+  },
+  isAdRunning: function() {
+    return window.CryptoSteamEmuSDK.isAdRunning();
+  },
+  reloadAd: function() {
+    window.CryptoSteamEmuSDK.reloadAd();
+  },
+  requestAd: function() {
+    return window.CryptoSteamEmuSDK.requestAd();
+  },
+  
+  
+  //----------------------------------------
+  //-- SDK Information
+  //----------------------------------------
+  
+  getVersion: function() {
+    var str = window.CryptoSteamSDK.getVersion();
+
+    var bufferSize = lengthBytesUTF8(str) + 1;
+    var buffer = _malloc(bufferSize);
+    stringToUTF8(str, buffer, bufferSize);
+    return buffer;
+  },
+   
+  //----------------------------------------
+  //-- Per-App Information
+  //----------------------------------------
+  
+  getConfig: function() {
+    var str = JSON.stringify(window.CryptoSteamSDK.getConfig().config);
+  
+    var bufferSize = lengthBytesUTF8(str) + 1;
+    var buffer = _malloc(bufferSize);
+    stringToUTF8(str, buffer, bufferSize);
+    return buffer;
+  },
+    
+   
+  //----------------------------------------
+  //-- User
+  //----------------------------------------
+  
+  getProfile: function(cb) {
+    window.CryptoSteamSDK.getProfile().then(response => {
+      
+      var str = JSON.stringify(response)
+      
+      var bufferSize = lengthBytesUTF8(str) + 1;
+      var buffer = _malloc(bufferSize);
+      stringToUTF8(str, buffer, bufferSize);
+      
+      dynCall_vi(cb, buffer);
+    });
+  },
+    
+  getBalance: function(cb) {
+    window.CryptoSteamSDK.getBalance().then(response => {
+       var str = response.toString()
+       
+       var bufferSize = lengthBytesUTF8(str) + 1;
+       var buffer = _malloc(bufferSize);
+       stringToUTF8(str, buffer, bufferSize);
+       
+       dynCall_vi(cb, buffer);
+    });
+  },
+
+  getLocale: function() {  
+    var str = window.CryptoSteamSDK.getLocale();
+    var bufferSize = lengthBytesUTF8(str) + 1;
+    var buffer = _malloc(bufferSize);
+    stringToUTF8(str, buffer, bufferSize);
+    return buffer
+  },
+
+  //----------------------------------------
+  //-- Share
+  //----------------------------------------
+    
+  showSharing: function(url, text) {
+      window.CryptoSteamSDK.showSharing(url, text)
+  },
+ 
+  getStartParam: function() {  
+    var str = window.CryptoSteamEmuSDK.getStartParam();
+    var bufferSize = lengthBytesUTF8(str) + 1;
+    var buffer = _malloc(bufferSize);
+    stringToUTF8(str, buffer, bufferSize);
+    return buffer
+  },
+  
+  //----------------------------------------
+  //-- IAP
+  //----------------------------------------
+  
+  openPurchaseConfirmModal: function(itemId, useRect, x, y, width, height, cb) {
+    window.CryptoSteamSDK.getShopItems().then(response => {
+    
+      const item = response.find(item => item.id === itemId)
+      
+      if(!item) {
+          const str = 'item not found';
+                   
+          const bufferSize = lengthBytesUTF8(str) + 1;
+          const buffer = _malloc(bufferSize);
+          stringToUTF8(str, buffer, bufferSize);
+          
+          dynCall_vi(cb, buffer);
+          return;
+      }
+
+      
+      var rect = null;
+      
+      if(useRect) {
+        rect = { x, y, width, height }; 
+      } 
+   
+      window.CryptoSteamSDK.openPurchaseConfirmModal(item, rect).then(response => {
+           
+           var str;
+           try {
+            str = JSON.stringify(response);
+           }
+           catch(ex) {
+            str = "{ \"status\": " + "\"error\"" + "}";
+           }
+           
+           var bufferSize = lengthBytesUTF8(str) + 1;
+           var buffer = _malloc(bufferSize);
+           stringToUTF8(str, buffer, bufferSize);
+           
+           dynCall_vi(cb, buffer);
+        }).catch(response => {
+           const str = "{ \"status\": " + "\"error\"" + "}";
+                      
+           var bufferSize = lengthBytesUTF8(str) + 1;
+           var buffer = _malloc(bufferSize);
+           stringToUTF8(str, buffer, bufferSize);
+           
+           dynCall_vi(cb, buffer);
+        });
+ 
+    })  
+  },
+
+  getShopItems: function(cb) {
+      window.CryptoSteamSDK.getShopItems().then(response => {
+         
+          var str = "{ \"items\": " + JSON.stringify(response) + "}";
+          
+          var bufferSize = lengthBytesUTF8(str) + 1;
+          var buffer = _malloc(bufferSize);
+          stringToUTF8(str, buffer, bufferSize);
+          
+          dynCall_vi(cb, buffer);
+      });
+  },
+    
+  getPurchasedShopItems: function(cb) {
+      window.CryptoSteamSDK.getPurchasedShopItems().then(response => {
+          
+          var str = JSON.stringify(response);
+          
+          var bufferSize = lengthBytesUTF8(str) + 1;
+          var buffer = _malloc(bufferSize);
+          stringToUTF8(str, buffer, bufferSize);
+          
+          dynCall_vi(cb, buffer);
+      });
+  },
+    
+
+  //----------------------------------------
+  //-- Cloud Saves
+  //----------------------------------------
+  
+  setValueSync: function(key, value) {
+    window.CryptoSteamEmuSDK.setValueSync(UTF8ToString(key), UTF8ToString(value))
+  },
+  
+  getValueSync: function(key, cb) {
+      var str = window.CryptoSteamEmuSDK.getValueSync(UTF8ToString(key))
+      
+      if(!str) {
+        str = "";
+      }
+      
+      var bufferSize = lengthBytesUTF8(str) + 1;
+      var buffer = _malloc(bufferSize);
+      stringToUTF8(str, buffer, bufferSize);
+      
+      return buffer;
+  },
+    
+  setValue: function(key, value) {
+    window.CryptoSteamSDK.setValue(UTF8ToString(key), UTF8ToString(value))
+  },
+
+  getValue: function(key, cb) {
+    window.CryptoSteamSDK.getValue(UTF8ToString(key)).then(response => {
+        var str = response;
+                  
+        var bufferSize = lengthBytesUTF8(str) + 1;
+        var buffer = _malloc(bufferSize);
+        stringToUTF8(str, buffer, bufferSize);
+        
+        dynCall_vi(cb, buffer);
+    }).catch(response => {
+        var str = "";
+                  
+        var bufferSize = lengthBytesUTF8(str) + 1;
+        var buffer = _malloc(bufferSize);
+        stringToUTF8(str, buffer, bufferSize);
+        
+        dynCall_vi(cb, buffer);
+    });
+  },
+ 
+  removeValue: function(key) {
+    window.CryptoSteamEmuSDK.removeValueSync(UTF8ToString(key));
+  },
+  
+  //----------------------------------------
+  //-- Achievements
+  //----------------------------------------
+  
+  
+  // not implemented
+  
+  //----------------------------------------
+  //-- Game Events
+  //----------------------------------------
+  
+  // not implemented
+  
+ 
+}
+
+addToLibrary(LIB);
---
+++ cryptosteamsdk/src/main.cpp
@@ -0,0 +1,294 @@
+// Extension lib defines
+#define EXTENSION_NAME cryptosteamsdk
+#define LIB_NAME "cryptosteamsdk"
+#define MODULE_NAME LIB_NAME
+
+// Defold SDK
+#define DLIB_LOG_DOMAIN LIB_NAME
+#include <dmsdk/sdk.h>
+
+#if defined(DM_PLATFORM_HTML5)
+
+#include <emscripten.h>
+
+//----------------------------------------
+//-- Startup Events
+//----------------------------------------
+extern "C" void gameReady();
+static int GameReady(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    gameReady();
+
+    lua_pushnil(L);
+
+    return 1;
+}
+//----------------------------------------
+//-- Advertisement
+//----------------------------------------
+// todo async methods
+// extern "C" bool isAdEnabled(cb);
+extern "C" bool isAdRunning();
+static int IsAdRunning(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    bool result = isAdRunning();
+
+   lua_pushboolean(L, result);
+
+    return 1;
+}
+
+extern "C" void reloadAd();
+static int ReloadAd(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    reloadAd();
+
+    lua_pushnil(L);
+
+    return 1;
+}
+
+extern "C" void requestAd();
+static int RequestAd(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    requestAd();
+
+    lua_pushnil(L);
+
+    return 1;
+}
+//----------------------------------------
+//-- SDK Information
+//----------------------------------------
+extern "C" const char* getVersion();
+static int GetVersion(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    const char* result = getVersion();
+
+    if (result == 0 || strcmp(result, "") == 0) {
+        lua_pushnil(L);
+    } else {
+        lua_pushstring(L, result);
+    }
+
+    return 1;
+}
+//----------------------------------------
+//-- Per-App Information
+//----------------------------------------;
+extern "C" const char* getConfig();
+static int GetConfig(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    const char* result = getConfig();
+
+    if (result == 0 || strcmp(result, "") == 0) {
+        lua_pushnil(L);
+    } else {
+        lua_pushstring(L, result);
+    }
+
+    return 1;
+}
+//----------------------------------------
+//-- User
+//----------------------------------------
+// todo async methods
+// extern "C" const char* getProfile(cb);
+// extern "C" long getBalance(cb)
+extern "C" const char* getLocale();
+static int GetLocale(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    const char* result = getLocale();
+
+    if (result == 0 || strcmp(result, "") == 0) {
+        lua_pushnil(L);
+    } else {
+        lua_pushstring(L, result);
+    }
+
+    return 1;
+}
+//----------------------------------------
+//-- Share
+//----------------------------------------
+extern "C" void showSharing(const char* url, const char* text);
+static int ShowSharing(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    const char* url = luaL_checkstring(L, 1);
+    const char* text = luaL_checkstring(L, 2);
+
+    showSharing(url, text);
+
+    lua_pushnil(L);
+
+    return 1;
+}
+extern "C" const char* getStartParam();
+static int GetStartParam(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+
+    const char* result = getStartParam();
+
+    if (result == 0 || strcmp(result, "") == 0) {
+        lua_pushnil(L);
+    } else {
+        lua_pushstring(L, result);
+    }
+
+    return 1;
+}
+//----------------------------------------
+//-- IAP
+//----------------------------------------
+// todo async methods
+// extern "C" void openPurchaseConfirmModal(itemId, useRect, x, y, width, height, cb);
+// extern "C" const char* getShopItems(cb);
+// extern "C" const char* getPurchasedShopItems(cb);
+//----------------------------------------
+//-- Cloud Saves
+//----------------------------------------
+extern "C" void setValueSync(const char* key, const char* value);
+static int SetValueSync(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* key = luaL_checkstring(L, 1);
+    const char* value = luaL_checkstring(L, 2);
+
+    setValueSync(key, value);
+
+    lua_pushnil(L);
+
+    return 1;
+}
+extern "C" const char* getValueSync(const char* key);
+static int GetValueSync(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* key = luaL_checkstring(L, 1);
+    
+    const char* result = getValueSync(key);
+
+    if (result == 0 || strcmp(result, "") == 0) {
+        lua_pushnil(L);
+    } else {
+        lua_pushstring(L, result);
+    }
+
+    return 1;
+}
+// todo async methods
+// extern "C" void setValue(const char* key, const char* v);
+// extern "C" const char* getValue(const char* key, cb);
+extern "C" void removeValue(const char* key);
+static int RemoveValue(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 1);
+    const char* key = luaL_checkstring(L, 1);
+
+    removeValue(key);
+
+    lua_pushnil(L);
+
+    return 1;
+}
+
+
+
+//
+// Functions exposed to Lua
+static const luaL_reg Module_methods[] =
+{
+    {"GameReady", GameReady},
+    {"IsAdRunning", IsAdRunning},
+    {"ReloadAd", ReloadAd},
+    {"RequestAd", RequestAd},
+    {"GetVersion", GetVersion},
+    {"GetConfig", GetConfig},
+    {"GetLocale", GetLocale},
+    {"ShowSharing", ShowSharing},
+    {"GetStartParam", GetStartParam},
+    {"SetValueSync", SetValueSync},
+    {"GetValueSync", GetValueSync},
+    {"RemoveValue", RemoveValue},
+    {0, 0}
+};
+
+
+//
+// Extension
+static void LuaInit(lua_State* L)
+{
+    int top = lua_gettop(L);
+
+    // Register lua names
+    luaL_register(L, MODULE_NAME, Module_methods);
+
+    lua_pop(L, 1);
+    assert(top == lua_gettop(L));
+}
+
+static dmExtension::Result AppInitializeExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeExtension(dmExtension::Params* params)
+{
+    // Init Lua
+    LuaInit(params->m_L);
+    printf("Registered %s Extension\n", MODULE_NAME);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+#else
+
+static dmExtension::Result AppInitializeExtension(dmExtension::AppParams* params)
+{
+    dmLogWarning("Registered %s (null) Extension\n", MODULE_NAME);
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+#endif
+
+DM_DECLARE_EXTENSION(EXTENSION_NAME, LIB_NAME, AppInitializeExtension, AppFinalizeExtension, InitializeExtension, 0, 0, FinalizeExtension)
